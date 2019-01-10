a783fd781b05c1da35039686292ece9008ec64fb
rodrigotorrico <rodrigo.torricodelcastillo@hotmail.com>
2019-01-10 18:24:49 -0400

    Implemented open door animation and animator

---
+++ UnityGGJ/Assets/Door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+
+	Animator animator;
+
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	void Update () {
+	}
+
+	void OnTriggerEnter( Collider other) {
+		if(other.gameObject.tag == "Player") {
+			animator.SetTrigger("Abrir");
+		}
+	}
+}
